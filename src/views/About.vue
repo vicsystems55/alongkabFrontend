9ed976f4e9b4e5e3c445fb97e5919255648c59e5
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2021-03-08 17:45:07 +0100

    created login and register end points

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,81 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="col-md-4 mx-auto mt-5">
+    <v-card
+      elevation="2"
+      class="spacing-playground pa-6"
+    fluid
+    >
+
+          <h1>Login</h1>
+          <v-text-field
+            label="Enter Email"
+            :rules="rules"
+            hide-details="auto"
+            v-model="email"
+          ></v-text-field>
+          <v-text-field 
+            type="password"
+            label="Enter Password"
+            v-model="password"
+          ></v-text-field>
+          <v-btn
+            color="primary"
+            depressed
+            elevation="2"
+            large
+            :loading='this.loadx'
+            v-on:click="login()"
+            
+          >LOGIN</v-btn>
+
+    </v-card>
   </div>
 </template>
+<script>
+import axios from 'axios'
+  export default {
+  created () {
+
+
+  },
+  methods: {
+
+     login() {
+
+
+
+      console.log(this.email)
+
+        axios
+        .post('http://localhost:1000/api/login', {
+          email: this.email,
+          password: this.password
+        })
+        .then(response => {
+          console.log(response);
+          this.$router.push('/') 
+        })
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
+      
+    }
+
+  },
+    data: () => ({
+
+      email: '',
+      password: '',
+      loadx: false,
+
+
+      
+      rules: [
+        value => !!value || 'Required.',
+        value => (value && value.length >= 3) || 'Min 3 characters',
+      ],
+    }),
+  }
+</script>
